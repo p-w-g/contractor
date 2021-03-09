34bae468a287fc0cc566c65c3fe8ff30b5b1a4b9
p-w-g <p.grzelczyk@gmail.com>
2021-03-09 13:38:09 +0100

    implement loading persistence on mount, add reset button

--- src/views/NewOffer.vue
+++ src/views/NewOffer.vue
@@ -2,10 +2,12 @@
   <div>
     <job-form />
     <overview />
+    <button class="fr__button--submit" @click="resetState">Reset</button>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import store from '@/store/index'
 
 import JobForm from '@/components/offer/JobForm.vue'
 import Overview from '@/components/offer/Overview.vue'
@@ -15,6 +17,17 @@ export default defineComponent({
   components: {
     JobForm,
     Overview
+  },
+  mounted() {
+    this.loadDataFromJson()
+  },
+  methods: {
+    loadDataFromJson() {
+      store.dispatch('attemptLoadJsonAction')
+    },
+    resetState() {
+      store.dispatch('resetStateAction')
+    }
   }
 })
 </script>
