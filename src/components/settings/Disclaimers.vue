2cc737cf2c56e69a0ff1e422beed6d22bf682d5e
p-w-g <p.grzelczyk@gmail.com>
2021-03-10 20:23:13 +0100

    scaffold disclaimers

---
+++ src/components/settings/Disclaimers.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="fr__heading">
+    <h3>Vilkor</h3>
+  </div>
+  <div class="fr__column-wrap">
+    <form class="fr__form" @submit.prevent="saveCompany">
+      <fieldset>
+        <!-- Faktura skickas med 7 dagar betalningstid efter uppfört arbete -->
+        <form-input
+          labelFull="Fakturering"
+          labelShort="fakturering"
+          v-model="fakturering"
+        />
+
+        <form-input
+          labelFull="Offert giltighetstid"
+          labelShort="giltig"
+          v-model="giltig"
+        />
+
+        <form-input
+          labelFull="Garanti"
+          labelShort="garanti"
+          v-model="garanti"
+        />
+
+        <form-input
+          labelFull="Arbetstid"
+          labelShort="arbetstid"
+          v-model="arbetstid"
+        />
+
+        <form-input
+          labelFull="Försäkring"
+          labelShort="försäkring"
+          v-model="försäkring"
+        />
+
+        <form-input
+          labelFull="Mindre avvikelse"
+          labelShort="avvikelse"
+          v-model="avvikelse"
+        />
+
+        <form-input
+          labelFull="Tillägtjänster"
+          labelShort="extra"
+          v-model="extra"
+        />
+
+        <form-input
+          labelFull="ROT / ROT vilkor"
+          labelShort="rot"
+          v-model="rot"
+        />
+
+        <button class="fr__button--submit">Spåra</button>
+      </fieldset>
+    </form>
+  </div>
+  <hr />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import store from '@/store/index'
+import FormInput from '../FormInput.vue'
+
+export default defineComponent({
+  name: 'Disclaimers',
+  components: {
+    FormInput
+  },
+  data: () => ({
+    fakturering: '',
+    giltig: '',
+    garanti: '',
+    arbetstid: '',
+    försäkring: '',
+    avvikelse: '',
+    extra: '',
+    rot: ''
+  }),
+  methods: {
+    saveDisclaimers() {
+      // TODO: implement save to store and persist
+    }
+  }
+})
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,18 +1,21 @@
 <template>
   <Foretag />
   <Kontakt />
+  <Disclaimers />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Foretag from '../components/settings/Foretag.vue'
 import Kontakt from '../components/settings/Kontakt.vue'
+import Disclaimers from '../components/settings/Disclaimers.vue'
 
 export default defineComponent({
   name: 'Settings',
   components: {
     Foretag,
-    Kontakt
+    Kontakt,
+    Disclaimers
   }
 })
 </script>
