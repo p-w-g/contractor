6c2a98e1750cc06eb3fb98e91d52a4b7de0c1dc0
p-w-g <p.grzelczyk@gmail.com>
2021-03-10 18:23:44 +0100

    implement reusable text input for consistency

---
+++ src/components/FormInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fr__label-wrapper">
+    <label :for="labelShort">{{ labelFull }}</label>
+    <input
+      :id="labelShort"
+      type="text"
+      class="fr__input-box"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'FormInput',
+  props: {
+    labelFull: String,
+    labelShort: String,
+    modelValue: String
+  }
+})
+</script>
