8a94f2492f24694e4e16c9c3452f39d72f2c6b69
p-w-g <p.grzelczyk@gmail.com>
2021-03-10 18:24:42 +0100

    add settings scaffold

---
+++ src/views/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <Foretag />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import store from '@/store/index'
+import Foretag from '../components/settings/Foretag.vue'
+
+export default defineComponent({
+  name: 'Settings',
+  components: {
+    Foretag
+  }
+})
+</script>
+
+<style></style>
