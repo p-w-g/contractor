6c53177cd63fdacbd8645ed272de88ac4d98d3d1
p-w-g <p.grzelczyk@gmail.com>
2021-03-18 14:01:59 +0100

    implement first page

--- src/components/preview/FirstPage.vue
+++ src/components/preview/FirstPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <p>first page</p>
+  <div class="page-container">
+    <div class="flex-container">
+      <img
+        class="printable-image"
+        :src="companyData.previewImage"
+        alt=""
+        srcset=""
+      />
+      <h3 class="printable-h3">
+        {{ this.companyData.leadin }}
+      </h3>
+    </div>
+  </div>
   <corpo-footer />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import store from '@/store'
+import { companyModel } from '@/store/models'
 import CorpoFooter from './CorpoFooter.vue'
 export default defineComponent({
-  name: 'FirstPage',  components: { CorpoFooter }
+  name: 'FirstPage',
+  components: { CorpoFooter },
+  computed: {
+    companyData(): companyModel {
+      return store.getters.company
+    }
+  }
 })
 </script>
