d71ac2f7c8f19e708aabf02bb3c328acfc308a56
p-w-g <p.grzelczyk@gmail.com>
2021-03-19 21:32:33 +0100

    restructrure first page

--- src/components/preview/FirstPage.vue
+++ src/components/preview/FirstPage.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="page-container">
     <div class="flex-container">
-      <img
-        class="printable-image"
-        :src="companyData.previewImage"
-        alt=""
-        srcset=""
-      />
-      <h3 class="printable-h3">
+      <img class="printable-image" :src="companyData.previewImage" />
+      <h3 class="printable-h3--leadin">
         {{ this.companyData.leadin }}
       </h3>
     </div>
   </div>
-  <corpo-footer />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import store from '@/store'
 import { companyModel } from '@/store/models'
-import CorpoFooter from './CorpoFooter.vue'
 export default defineComponent({
   name: 'FirstPage',
-  components: { CorpoFooter },
   computed: {
     companyData(): companyModel {
       return store.getters.company
