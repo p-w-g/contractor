40e4ac31cd9fcb1282a65f5d3e43fc3ca43b212e
p-w-g <p.grzelczyk@gmail.com>
2021-05-29 23:06:13 +0200

    FR-60 add leadin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    Home Route
+    <header class="banner-logo">Contractor</header>
+
+    <article>Öfferteringsappen avsedd för folk inom hantverk.</article>
+
+    <!-- <article>navigation, drawer</article>
+    <article>settings: general info about settings</article>
+    <article>settings: info about company</article>
+    <article>settings: info about rep</article>
+    <article>settings: misc info aka disclaimers</article> -->
   </div>
 </template>
 
